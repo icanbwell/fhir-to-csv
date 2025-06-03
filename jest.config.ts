499792ef0f65d52f4028ea6052fc7d00188da278
Imran Qureshi <imranq2@hotmail.com>
2025-06-03 18:31:41 +0200

    rename jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,19 +1,12 @@
 import {createDefaultPreset, JestConfigWithTsJest} from 'ts-jest'
 
 const presetConfig = createDefaultPreset({
-    // Equivalent to the previous 'globals' configuration
-    diagnostics: {
-        // Disable type checking diagnostics
-        warnOnly: true,
-    },
-    // Optional: Equivalent to isolatedModules: true
-    isolatedModules: true,
-})
+});
 
 const jestConfig: JestConfigWithTsJest = {
     ...presetConfig,
     testEnvironment: 'node',
-    testMatch: ['**/test/**/*.spec.ts'],
+    testMatch: ['**/test/**/*.spec.ts','**/test/**/*.ts'],
     collectCoverageFrom: [
         '<rootDir>/src/**/*.ts',
         '!<rootDir>/src/types/**/*.ts',
--- src/converters/binary_extractor.ts
+++ src/converters/binary_extractor.ts
@@ -1,6 +1,7 @@
 // `src/converters/binary_extractor.ts`
 import { BaseResourceExtractor, ExtractorValueType } from './base_extractor';
 import { TBinary } from '../types/resources/Binary';
+import TextConverter from '../utils/textConverter';
 
 export class BinaryExtractor extends BaseResourceExtractor<TBinary> {
   extract(binary: TBinary): Record<string, ExtractorValueType> {
@@ -15,16 +16,19 @@ export class BinaryExtractor extends BaseResourceExtractor<TBinary> {
   }
 
   convertContent(contentType: string, data: string | undefined) {
+    if (!data) {
+      return undefined; // No data to convert
+    }
     //write code to convert content to plain text and use content type to determine the encoding
     // content-type is either plain text, rtf or html
     if (contentType === 'text/plain') {
       return data;
     } else if (contentType === 'text/rtf') {
       // Convert RTF to plain text
-      return this.convertRtfToPlainText(data);
+      return TextConverter.convertRtfToText(data);
     } else if (contentType === 'text/html') {
       // Convert HTML to plain text
-      return this.convertHtmlToPlainText(data);
+      return TextConverter.convertHtmlToText(data);
     }
     return undefined;
   }
